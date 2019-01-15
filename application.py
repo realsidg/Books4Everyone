4265528e86b5afd7e086f7e25ab56415d3600431
sgarg510 <i@siddharthgarg.in>
2019-01-15 17:53:46 +0530

    Updates template folder

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,7 +24,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
 
 @app.route("/book/<string:isbn>")
 def book(isbn):
